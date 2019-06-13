b76ead0a49e3ded2ad3890e4c8d7f6d49f351174
Hannah Mudge <Heenawter@users.noreply.github.com>
2019-06-13 10:41:30 -0600

    first player advantage...

--- C++/Constants.h
+++ C++/Constants.h
@@ -2,6 +2,8 @@
 
 #ifndef CONSTANTS
 
+#include <string>
+
 static const char RAN_OUT_OF_CARDS = 'c';
 static const char RAN_OUT_OF_TIME = 't';
 static const char SUCCESSFUL_CARD_DRAW = 's';
@@ -11,12 +13,15 @@ static const char WIN = 'w';
 static const char UNICORN = 0, GARGOYLE = 1, REVERSE = 2, ROTATE_R = 3, ROTATE_L = 4, DOUBLE = 5,
                   APPEND_1 = 6, APPEND_2 = 7, APPEND_3 = 8, REMOVE_1 = 9, REMOVE_2 = 10, REMOVE_3 = 11;
 
-static const int NUM_PLAYERS = 6;
-static const int NUM_ROUNDS = 500;
+static const int NUM_PLAYERS = 5;
+static const int NUM_ROUNDS = 100;
 
 static const int NUM_LINES_TO_SKIP = 24;
 
-static const char *INPUT_FILE = "Simulations/Draft1_Stats.txt";
-static const char *OUTPT_FILE = "Simulations/Draft1_Results_v5 (500 Rounds).txt";
+static const std::string DRAFT = "1";
+static const std::string INPUT_FILE = "Simulations/Draft" + DRAFT +"_Stats.txt";
+static const std::string OUTPT_FILE = "Simulations/Draft" + DRAFT + "_Results_v9 ("
+    + std::to_string(NUM_ROUNDS) + " Rounds, "
+    + std::to_string(NUM_PLAYERS) + " Players).txt";
 
-#endif
+#endif  
