fcc5582a4165ddbe342fc9c6b934889b589afa33
Hannah Mudge <Heenawter@users.noreply.github.com>
2019-06-13 18:16:50 -0600

    about to GET RID OF THOSE PERMUTATIONS boy

--- C++/Constants.h
+++ C++/Constants.h
@@ -13,14 +13,14 @@ static const char WIN = 'w';
 static const char UNICORN = 0, GARGOYLE = 1, REVERSE = 2, ROTATE_R = 3, ROTATE_L = 4, DOUBLE = 5,
                   APPEND_1 = 6, APPEND_2 = 7, APPEND_3 = 8, REMOVE_1 = 9, REMOVE_2 = 10, REMOVE_3 = 11;
 
-static const int NUM_PLAYERS = 5;
+static const int NUM_PLAYERS = 3;
 static const int NUM_ROUNDS = 100;
 
 static const int NUM_LINES_TO_SKIP = 24;
 
 static const std::string DRAFT = "1";
 static const std::string INPUT_FILE = "Simulations/Draft" + DRAFT +"_Stats.txt";
-static const std::string OUTPT_FILE = "Simulations/Draft" + DRAFT + "_Results_v9 ("
+static const std::string OUTPT_FILE = "Simulations/Draft" + DRAFT + "_Results_v10 ("
     + std::to_string(NUM_ROUNDS) + " Rounds, "
     + std::to_string(NUM_PLAYERS) + " Players).txt";
 
