e16a876d36cb3f9e46633d8531f1f7145a29ad50
Hannah Mudge <hannah.k.mudge@gmail.com>
2020-01-15 13:46:30 -0700

    starting analysis

--- Better Design/Analysis/testAnalysis.py
+++ Better Design/Analysis/testAnalysis.py
@@ -1,10 +1,29 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-df = pd.read_csv("../GameInfo/results.csv")
-
-meanRoundsPerGoal = df[["Goal", "Number of Rounds"]].groupby("Goal").mean().reset_index()
+import math
 
 
-print(meanRoundsPerGoal)
+def plotBar_meanRoundsPerGoal():
+    df = pd.read_csv("../GameInfo/results.csv")
+
+    meanRoundsPerGoal = df[["Goal", "Number of Rounds"]].groupby("Goal").mean().reset_index()
+    # meanRoundsPerGoal.sort_values("Number of Rounds", inplace=True, ascending=False);
+
+    meanRoundsPerGoal["Card Count"] = meanRoundsPerGoal["Goal"].str.len() / 3
+    meanRoundsPerGoal["Unicorn Count"] = meanRoundsPerGoal.Goal.str.count('\*')
+    meanRoundsPerGoal.sort_values(
+        ["Card Count", "Unicorn Count"], inplace=True, ascending=False)
+
+    def getColour(num):
+        colourDict = {0: "#2ecc71", 1: "#e74c3c", 2: "#34495e"}
+        return colourDict[num]
+    
+    colours = meanRoundsPerGoal.apply(lambda x: getColour(x["Unicorn Count"]), axis=1)
+
+    fig = meanRoundsPerGoal.plot(x="Goal", y="Number of Rounds", kind="barh", color=colours)
+    fig.set_xlabel("Mean Number of Rounds")
+    plt.show()
+
+
+plotBar_meanRoundsPerGoal()
